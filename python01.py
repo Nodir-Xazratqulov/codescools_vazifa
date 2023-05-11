833a4305c0a12e5a8db1e0522c86534c3c9daa39
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-11 14:44:15 +0500

    eight digit

--- python01.py
+++ python01.py
@@ -1,16 +1,20 @@
-a = 123
-b = a%10
-s = a//10
-d = s%10
-e = s//10
-print(d)
+a = 2567
+b = a%10 
+s = a//10 
+d = s%10 
+e = s//10 
+g = e%10
+j = e//10  
+print(b,d,g,j, sep='')
 
 
-b = 351
-f = b%10
-s = b//10
-d = s%10
-e = s//10
-print(d)
+k = 8745
+b = k%10 
+s = k//10 
+d = s%10 
+e = s//10 
+g = e%10
+j = e//10  
+print(b,d,g,j, sep='')
 
 
